c5b763dddb07a52e4d25e36a04162da7380ab7bc
goott7-8 <goott7-8@DESKTOP-99L19FI>
2020-06-23 14:29:10 +0900

    프로그래머스 숫자의 표현

---
+++ algo/src/algo/Solution2.java
@@ -0,0 +1,32 @@
+package algo;
+
+class Solution2 {
+    public int solution(int n) {
+        int answer = 0;
+        
+        
+        for(int i = 1 ; i <= n;i++) {
+        	
+        	for(int j = i+1; j <= n; j++) {
+        		int sum = i;
+        		
+        		if(sum == n) {
+        			answer++;
+        		}else if (sum > n) {
+        			break;
+        		}else {
+        			if(sum < n) {
+        				sum = sum+j;
+        			}
+        		}
+        		
+        		
+        		
+        	}//for end
+        	
+        }// for end
+        
+        
+        return answer;
+    }
+}
