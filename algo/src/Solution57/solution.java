aed05140bdde3f742ccee55b43a82e5da1748d7b
ys3135 <ys3135@gmail.com>
2020-11-09 16:57:24 +0900

    프로그래머스 소수 만들기( 조합X) 3중for문 통과

--- algo/src/Solution57/solution.java
+++ algo/src/Solution57/solution.java
@@ -15,44 +15,55 @@ public class solution {
 
         visited = new boolean[nums.length];
         
-        combi(nums,0,nums.length,3);
+        for(int i = 0 ; i < nums.length;i++) {
+        	for(int j = i+1 ; j < nums.length; j++) {
+        		for(int k = j+1 ; k < nums.length; k++) {
+        			int num = nums[i]+nums[j]+nums[k];
+        				if(sosu(num)) {
+        					answer ++;
+        				}
+        			
+        		}
+        	}
+        }
+        
         
         return answer;
     }
     
     
-    public static void combi(int arr[] ,int start,int n, int r) {
-    	if( r== 0 ) {
-    		int num = 0;
-    		int cnt = 0 ;
-    		for(int i = 0 ; i < n ; i++) {
-    			if(visited[i]) {
-    				System.out.print(arr[i] + " ");
-    				num += arr[i];
-    			}
-    		}
-    		System.out.println();
-    		// 소수 판별
-    		if(sosu(num)) {
-    			answer++;
-    		}
-    	}
-    	
-    	for(int i = start ; i < n ; i ++) {
-    		visited[i] = true;
-    		combi(arr,i+1,n,r-1);
-    		visited[i] = false;
-    	}
-    	
-    	
-    }
+//    public static void combi(int arr[] ,int start,int n, int r) {
+//    	if( r== 0 ) {
+//    		int num = 0;
+//    		int cnt = 0 ;
+//    		for(int i = 0 ; i < n ; i++) {
+//    			if(visited[i]) {
+//    				System.out.print(arr[i] + " ");
+//    				num += arr[i];
+//    			}
+//    		}
+//    		System.out.println();
+//    		// 소수 판별
+//    		if(sosu(num)) {
+//    			answer++;
+//    		}
+//    	}
+//    	
+//    	for(int i = start ; i < n ; i ++) {
+//    		visited[i] = true;
+//    		combi(arr,i+1,n,r-1);
+//    		visited[i] = false;
+//    	}
+//    	
+//    	
+//    }
     
     
     public static boolean sosu(int num) {
     	
     	int sqrt = (int) Math.sqrt(num);
     	
-    	for(int i = 4 ; i <= sqrt; i++) {
+    	for(int i = 2 ; i <= sqrt; i++) {
     		if(num%i == 0) {
     			return false;
     		}
