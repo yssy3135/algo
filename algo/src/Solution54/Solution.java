86d6842df6d34804b045ef6d6f0748ec787efedf
ys3135 <ys3135@gmail.com>
2020-11-09 13:09:19 +0900

    프로그래머스 포켓몬 (hashset을 이용한 중복제거)

--- algo/src/Solution54/Solution.java
+++ algo/src/Solution54/Solution.java
@@ -6,49 +6,34 @@ import java.util.HashSet;
 public class Solution {
 	
 	
-	static boolean visited[];
-	static int answer;
-	public static void main(String[] args) {
-		int arr[] = {3,3,3,2,2,4};
-		solution(arr);
-	}
+	
+
+
 	
     public static int solution(int[] nums) {
-        answer = 0;
+        int answer = 0;
         
-       visited = new boolean[nums.length];
-       
-       combi(nums,0,nums.length,nums.length/2);
+        int size = nums.length/2;
+        
+        HashSet<Integer> set = new HashSet<Integer>();
+        for(int i = 0 ; i < nums.length;i++) {
+        	set.add(nums[i]);
+        }
+        
+        
+        if(size<set.size()) {
+        	answer = size;
+        }else {
+        	answer = set.size();
+        }
+     
        
       
         
         return answer;
     }
     
-    public static void combi(int arr[],int start,int n,int r) {
-    	if(r == 0 ) {
-    		HashSet<Integer> set = new HashSet<Integer>();
-    		for(int i = 0 ; i < arr.length;i++) {
-    			if(visited[i]) {
-    				set.add(arr[i]);
-    				
-    			}
-    		}
-    	
-    		answer = Math.max(answer, set.size());
-    		
-    	}
-    	
-    	for(int i = start ; i < arr.length;i++) {
-    		visited[i] = true;
-    		combi(arr,i+1,n,r-1);
-    		visited[i] = false;
-    	}
-    	
-    	
-    	
-    }
-    
+
     
    
 	
