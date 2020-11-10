75b9256e5fa7f49501065ff8b54a628c00524db0
ys3135 <ys3135@gmail.com>
2020-11-10 13:53:14 +0900

    프로그래머스 점프와 순간이동 ( 복습 필요 )

---
+++ algo/src/Solution59/Solution.java
@@ -0,0 +1,36 @@
+package Solution59;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Solution {
+	public static void main(String[] args) {
+		
+		solution(5);
+		
+	}
+	
+    public static int solution(int n) {
+        int ans = 0;
+        
+        
+        while(n != 0 ) {
+        	if(n%2 == 0) {
+        		n =  n /2;
+        	}else {
+        		n = n-1;
+        		ans ++;
+        	}
+        	
+        }
+        
+
+        return ans;
+    }
+    
+    
+  
+    
+    
+    
+}
