8fbac013cda1ee21c404d78023e5d2665afb52a0
ys3135 <ys3135@gmail.com>
2020-12-30 23:58:22 +0900

    백준 1726 로봇(bfs) 복습필수

--- algo/src/Solution84/Main.java
+++ algo/src/Solution84/Main.java
@@ -1,97 +1,128 @@
 package Solution84;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.Scanner;
+import java.util.StringTokenizer;
+
 
 public class Main {
 	
 	static int map[][];
 
-	static int a[] = {0,0,1,-1};
-	static int b[] = {1,-1,0,0};
+	static int a[] = {0,0,0,1,-1};
+	static int b[] = {0,1,-1,0,0};
 	static int n;
 	static int m;
-	static int gocnt;
-	static boolean visited[][];
+	static int start[];
+	static int end[];
+	static boolean visited[][][];
 	
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		n =sc.nextInt();
-		m = sc.nextInt();
-		gocnt = 0;
+		String tmp[] = br.readLine().split(" ");
+		 n = Integer.parseInt(tmp[0]);
+		 m = Integer.parseInt(tmp[1]);
+		
 		map = new int[n][m];
-		visited = new boolean[n][m];
+		visited = new boolean[n][m][5];
+		
 		
 		for(int i = 0 ; i < n ;i++) {
-			for(int j = 0;j < m ;j++ ) {
-				map[i][j] = sc.nextInt();
+			String input[] = br.readLine().split(" ");
+			for(int j = 0 ; j < input.length;j++) {
+				map[i][j] = Integer.parseInt(input[j]);
 			}
 		}
 		
-		int start[] = new int[3];
-		for(int i = 0 ; i < 3; i++) {
-			start[i] = sc.nextInt()-1;
-		}
-		int end[] = new int[3];
-		for(int i = 0 ; i < 3; i++) {
-			end[i] = sc.nextInt()-1;
-		}
-		map[start[0]][start[1]] = 0;
 		
-		bfs(start,end);
+		tmp = br.readLine().split(" ");
 		
-//		for(int i = 0 ; i < n ;i++) {
-//			System.out.println(Arrays.toString(map[i]));
-//		}
-		System.out.println(map[end[0]][end[1]]);
+		start = new int[tmp.length];
+		for(int  i= 0  ; i< tmp.length;i++) {
+			start[i] = Integer.parseInt(tmp[i]);
+		}
+		
+		tmp = br.readLine().split(" ");
+		
+		end = new int[tmp.length];
+		for(int  i= 0  ; i< tmp.length;i++) {
+			end[i] = Integer.parseInt(tmp[i]);
+		}
+		
+		bfs(start);
+		
 		
 	}
 	
-	public static void bfs(int start[],int end[]) {
+	public static void bfs(int info[]) {
 		Queue<int[]> que = new LinkedList<int[]>();
 		
-		que.add(start);
 		
+		visited[info[0]-1][info[1]-1][info[2]] = true;
+		que.add(new int[] {info[0]-1,info[1]-1,info[2],0});
+
 		while(!que.isEmpty()) {
-			int now[] = que.poll();
-			visited[now[0]][now[1]] = true;
-			int dir = now[2];
-			for(int i = 0 ; i < 4 ;i++) {
-				
-				int plus = 0;
-				if(dir == i) {
-					
-				}else if(dir + i == 1 || dir + i ==5 ) {
-					plus = 2;
-				
-				}else {
-					plus = 1;
-				}
-				for(int j = 1;  j <= 3 ; j++) {
-					int mx = now[0] + a[i]*j;
-					int my = now[1] + b[i]*j;
-					if(mx >= 0 && my >= 0 && mx< n && my <m) {
-						if(map[mx][my] == 0 && !visited[mx][my]) {
-							map[mx][my] = map[now[0]][now[1]]+1+plus;
-							que.add(new int[] {mx,my,i});
-						}else {
-							break;
-						}
-					}
-					if(mx == end[0] && my == end[1] && dir == end[2]) {
-						map[mx][my] = map[end[0]][end[1]]+plus;
-						return ;
-					}
-				}
-				for(int z = 0 ; z < n ;z++) {
-					System.out.println(Arrays.toString(map[z]));
-				}
-				
-				System.out.println("================================");
+			int robot[] = que.poll();
+			int x = robot[0];
+			int y = robot[1];
+			int dir = robot[2];
+			int cnt = robot[3];
+		
+			if(x == end[0]-1 && y == end[1]-1 && dir == end[2]) {
+				System.out.println(cnt);
+			
+				return ;
 			}
+			
+			for(int i = 1; i <=3 ; i++) {
+				
+				int mx = x + (a[dir]*i);
+				int my = y + (b[dir]*i);
+				
+
+				if(mx >= 0 && my >= 0 && mx < n && my < m) {
+					
+					if(map[mx][my] == 0 &&  !visited[mx][my][dir]) {
+				
+						visited[mx][my][dir] = true;
+						que.add(new int[] {mx,my,dir,cnt+1});
+						
+					}else {
+						break;
+					}
+				}
+				
+				
+				
+			}
+			
+			for(int i = 1 ; i <=4 ; i++) {
+				if(!visited[x][y][i] && dir != i) {
+					visited[x][y][i] = true;
+					
+					if (dir+i == 3 || dir+i == 7) {
+					
+						que.add(new int[] {x,y,i,cnt+2});
+					}else {
+					
+						que.add(new int[] {x,y,i,cnt+1});
+					}
+					
+				}
+				
+	
+			}
+			
+			
+			
 			
 			
 		}
@@ -99,6 +130,10 @@ public class Main {
 		
 		
 	}
+	
+	
+
+	
 	
 
 	
