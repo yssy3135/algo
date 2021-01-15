5e133105363d766632bdf98d13bc15647968f2cd
ys3135 <ys3135@gmail.com>
2021-01-15 12:33:19 +0900

    백준 15683 감시(dfs,구현) 다시 꼭 풀기

--- algo/src/algostudy_14_BOJ_15683/Main.java
+++ algo/src/algostudy_14_BOJ_15683/Main.java
@@ -17,7 +17,7 @@ public class Main {
 		int y;
 		int type;
 		
-		public CCTV(int x, int y, int type) {
+		public CCTV(int y, int x, int type) {
 			super();
 			this.x = x;
 			this.y = y;
@@ -29,6 +29,9 @@ public class Main {
 	static ArrayList<CCTV> list;
 	static int N;
 	static int M;
+	static int rot_size[] = {4,2,4,4,1};
+	static int ans;
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
@@ -36,7 +39,7 @@ public class Main {
 		
 		N = Integer.parseInt(input[0]);
 		M = Integer.parseInt(input[1]);
-		
+		ans = 100;
 		map = new int[N][M];
 		
 		list = new ArrayList<CCTV>();
@@ -44,300 +47,128 @@ public class Main {
 			input = br.readLine().split(" ");
 			for(int j = 0 ; j < input.length; j++) {
 				int num = Integer.parseInt(input[j]);
-				if(num != 0) {
+				if(num != 0 && num != 6) {
 					list.add(new CCTV(i, j, num));
 				}
 				map[i][j] =	num;
 			}
 			
 		}
-		// 타입의 역순으로 정렬을 한뒤 cctv의 감시범위가 가장 큰방향으로 감시 
 		
-		list.sort(new Comparator<CCTV>() {
-			@Override
-			public int compare(CCTV o1, CCTV o2) {
-				// TODO Auto-generated method stub
-				return o1.type - o2.type;
-			}
-		});
+		dfs(0);
 		
+	
+		System.out.println(ans);
 		
-		for(int i = 0 ; i < list.size(); i++) {
-			
-			
-//			for(int q =0  ;q < map.length;q++) {
-//				System.out.println(Arrays.toString(map[q]));
-//			}
-//			
-			
-			
-			int copymap[][] = mapcopy();
-			CCTV cctv = list.get(i);
-			// 복사를 했어
+	
+		
 
-			
-			
-			if(cctv.type == 5) {
-				south(cctv, map);
-				left(cctv, map);
-				right(cctv, map);
-				north(cctv, map);
-			}else if(cctv.type == 4) {
-				//감시구역의 최대값과 어느 방향인지 알아야함
-				int s = south(cctv, copymap);
-				int l = left(cctv, copymap);
-				int r = right(cctv, copymap);
-				int n = north(cctv, copymap);
-				
-				int arr[] = {r+l+s,r+l+n,s+n+r,s+n+l};
-				int max = 0;
-				int idx = -1;
-				for(int j = 0 ; j < arr.length; j++) {
-					if(max < arr[j]) {
-						max = arr[j];
-						idx = j;
+
+	}
+	
+	public static void mapcopy(int arr[][],int copy[][]){
+		for(int i = 0 ; i < arr.length;i++) {
+			for(int j = 0 ; j < arr[i].length; j++) {
+				arr[i][j] = copy[i][j];
+			}
+		}
+	}
+	
+	
+	public static void dfs(int idx) {
+		
+		if(idx == list.size()) { // 다 돌았다..
+			int cnt = 0;
+			for(int i = 0 ; i < N;i++) {
+				for(int j = 0 ; j <M; j++) {
+					if(map[i][j] == 0) {
+						cnt++;
 					}
 				}
-				
-				if(idx == 0) {//r,l,s
-					right(cctv, map);
-					left(cctv, map);
-					south(cctv, map);
-				}else if(idx ==1) {//r,l,n
-					right(cctv, map);
-					left(cctv, map);
-					north(cctv, map);
-				}else if(idx ==2) {//s,n,r;
-					south(cctv, map);
-					north(cctv, map);
-					right(cctv, map);
-					
-				}else if(idx == 3) {
-					south(cctv, map);
-					north(cctv, map);
-					left(cctv, map);
-				}
-			}else if(cctv.type == 3) {
-				//감시구역의 최대값과 어느 방향인지 알아야함
-				int s = south(cctv, copymap);
-				int l = left(cctv, copymap);
-				int r = right(cctv, copymap);
-				int n = north(cctv, copymap);
-				
-				int arr[] = {n+r,l+n,l+s,s+r};
-				int max = 0;
-				int idx = -1;
-				for(int j = 0 ; j < arr.length; j++) {
-					if(max < arr[j]) {
-						max = arr[j];
-						idx = j;
-					}
-				}
-				
-				if(idx == 0) {//n+r
-					north(cctv, map);
-					right(cctv, map);
-				}else if(idx ==1) {//l+n
-					left(cctv, map);
-					north(cctv, map);
-				}else if(idx ==2) {//l+s
-					left(cctv, map);
-					south(cctv, map);
-				}else if(idx ==3) {//s+r
-					south(cctv, map);
-					right(cctv, map);
-				}
-				
-				
-				
-			}else if(cctv.type == 2) {
-				//감시구역의 최대값과 어느 방향인지 알아야함
-				int s = south(cctv, copymap);
-				int l = left(cctv, copymap);
-				int r = right(cctv, copymap);
-				int n = north(cctv, copymap);
-				
-				int arr[] = {r+l,s+n};
-				int max = 0;
-				int idx = -1;
-				for(int j = 0 ; j < arr.length; j++) {
-					if(max < arr[j]) {
-						max = arr[j];
-						idx = j;
-					}
-				}
+			}
+			
+			if(ans > cnt) {
+				ans = cnt;
+			}
+			return;
+		}
+		
 
-				if(idx == 0) {//r+l
-					right(cctv, map);
-					left(cctv, map);
-				}else if(idx ==1) {//n+s
-					north(cctv, map);
-					south(cctv, map);
+		
+		int backup[][] = new int[N][M];
+		int type = list.get(idx).type-1;
+		CCTV cctv = list.get(idx);
+		for(int i = 0 ; i < rot_size[type]; i++) {
+			mapcopy(backup, map);
+			if(type == 0) { //
+				update(i,cctv);
+			}else if (type == 1) {
+				update(i,cctv);
+				update(i+2,cctv);
+			}else if (type == 2) {
+				update(i,cctv);
+				update(i+1,cctv);
+			}else if (type == 3) {
+				update(i,cctv);
+				update(i+1,cctv);
+				update(i+2,cctv);
+			}else if (type == 4) {
+				update(i,cctv);
+				update(i+1,cctv);
+				update(i+2,cctv);
+				update(i+3,cctv);
+			}
+			
+			dfs(idx+1);
+			mapcopy(map, backup);
+			//원상복구
+		}
+		
+		
+	}
+	
+	public static void update(int dir,CCTV cctv) {		
+		dir = dir %4;
+		// 동서남북
+		if(dir == 0) { 
+			for(int x = cctv.x; x < M; x++) {
+				if(map[cctv.y][x] == 6) {
+					break;
+				}else {
+					map[cctv.y][x] = 9;
 				}
-				
-				
-				
-				
-			}else if(cctv.type == 1) {
-				//감시구역의 최대값과 어느 방향인지 알아야함
-				int s = south(cctv, copymap);
-				int l = left(cctv, copymap);
-				int r = right(cctv, copymap);
-				int n = north(cctv, copymap);
-				
-				int arr[] = {r,l,s,n};
-				int max = 0;
-				int idx = -1;
-
-				for(int j = 0 ; j < arr.length; j++) {
-					if(max < arr[j]) {
-						max = arr[j];
-						idx = j;
-					}
+			}
+		}else if (dir == 1) {
+			for(int y = cctv.y; y >= 0; y--) {
+				if(map[y][cctv.x] == 6) {
+					break;
+				}else {
+					map[y][cctv.x] = 9;
 				}
-
-				if(idx == 0) {//r
-					right(cctv, map);
-				}else if(idx ==1) {//l
-					left(cctv, map);
-				}else if(idx ==2) {//s
-					south(cctv, map);
-				}else if(idx ==3) {//n
-					north(cctv, map);
+			}
+		}else if (dir == 2) {
+			for(int x = cctv.x; x >= 0; x--) { // 북
+				if(map[cctv.y][x] == 6) {
+					break;
+				}else {
+					map[cctv.y][x] = 9;
 				}
-				
-				
 			}
 			
 			
+		}else if (dir == 3) {
+			for(int y = cctv.y; y < N; y++) { // 남
+				if(map[y][cctv.x] == 6) {
+					break;
+				}else {
+					map[y][cctv.x] = 9;
+				}
+			}
 			
-			
-		}// for end
-		
-		
-		for(int i =0  ;i < map.length;i++) {
-			System.out.println(Arrays.toString(map[i]));
 		}
-		
-		int cnt = 0;
+	}
 	
-		for(int i = 0 ;i  < map.length; i++) {
-			for(int j = 0 ; j < map[i].length; j++) {
-				if(map[i][j] == 0) {
-					cnt++;
-				}
-			}
-		}
-		
-		
-		
-		System.out.println(cnt);
+	
 
-	}
-	
-	public static int[][] mapcopy(){
-		int copy[][] = new int[N][M];
-		for(int i = 0 ; i < map.length;i++) {
-			for(int j = 0 ; j < map[i].length; j++) {
-				copy[i][j] = map[i][j];
-			}
-		}
-		
-		return copy;
-		
-	}
-	
-	
-	public static int left(CCTV cctv,int copymap[][]) {
-		
-		int x = cctv.x;
-		int y = cctv.y;
-		
-		int cnt = 0;
-		while(true) {
-			y -= 1;
-			if(y>=0 ) {
-				if(copymap[x][y] == 9 ||copymap[x][y] == 1||copymap[x][y] == 2||copymap[x][y] == 3||copymap[x][y] == 4||copymap[x][y] == 5) {
-					continue;
-				}else if(copymap[x][y] == 0  ) {
-					copymap[x][y] = 9;
-					cnt++;	
-				}else {
-					return cnt;
-				}
-			}else {
-				return cnt;
-			}
-		}
-
-	}
-	
-	public static int right(CCTV cctv,int copymap[][]) {
-		int x = cctv.x;
-		int y = cctv.y;
-		
-		int cnt = 0;
-		while(true) {
-			y += 1;
-			if(y <M ) {
-				if(copymap[x][y] == 9 ||copymap[x][y] == 1||copymap[x][y] == 2||copymap[x][y] == 3||copymap[x][y] == 4||copymap[x][y] == 5) {
-					continue;
-				}else if(copymap[x][y] == 0 ) {
-					
-					copymap[x][y] = 9;
-					cnt++;	
-				}else {
-					return cnt;
-				}
-			}else {
-				return cnt;
-			}
-		}
-	}
-	public static int south(CCTV cctv,int copymap[][]) {
-		
-		int x = cctv.x;
-		int y = cctv.y;
-		
-		int cnt = 0;
-		while(true) {
-			x += 1;
-		
-			if(x <N ) {
-				if(copymap[x][y] == 9 ||copymap[x][y] == 1||copymap[x][y] == 2||copymap[x][y] == 3||copymap[x][y] == 4||copymap[x][y] == 5) {
-					continue;
-				}else if(copymap[x][y] == 0  ) {
-					copymap[x][y] = 9;
-					cnt++;	
-				}else {
-					return cnt;
-				}
-			}else {
-				return cnt;
-			}
-		}
-	}
-	public static int north(CCTV cctv,int copymap[][]) {
-		int x = cctv.x;
-		int y = cctv.y;
-
-		int cnt = 0;
-		while(true) {
-			x -= 1;
-			if(x >=0 ) {
-				if(copymap[x][y] == 9 ||copymap[x][y] == 1||copymap[x][y] == 2||copymap[x][y] == 3||copymap[x][y] == 4||copymap[x][y] == 5) {
-					continue;
-				}else if(copymap[x][y] == 0 ) {
-					copymap[x][y] = 9;
-					cnt++;	
-				}else {
-					return cnt;
-				}	
-			}else {
-				return cnt;
-			}
-		}
-	}
-	
 	
 }
