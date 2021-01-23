392d94cfb36803c4f2a37d945c42d60891e9b755
ys3135 <ys3135@gmail.com>
2021-01-23 22:47:18 +0900

    백준 1941 소문난 칠공주 첫번째(메모리과다)

--- algo/src/solution89/Main.java
+++ algo/src/solution89/Main.java
@@ -3,10 +3,6 @@ package solution89;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
 
 public class Main {
 	
@@ -35,7 +31,7 @@ public class Main {
 		}
 		
 		
-		select(num,0,0,new int[] {0,0,0,0,0,0,0});
+		select(num,0,0,0);
 	
 		
 		System.out.println(ans);
@@ -49,26 +45,74 @@ public class Main {
 	
 	
 	// 7명의 숫자를 뽑는다.
-	public static void select(boolean num[], int cnt,int start) {
-		if(cnt == 6) {
-			
-			
-			
+	public static void select(boolean num[], int cnt,int start,int cnty) {
+		
+		
+		if(cnty>=4) {
 			return;
 		}
-		System.out.println("재귀"+cnt);
-		for(int i = start ; i < 25 ; i++) {
-			if(!num[i]) {
-				num[i] = true;
-				select(num,cnt+1,start+1);
-				num[i] = false;
+		
+		if(cnt == 7) {
+			int point = 0 ;
+			visited = new boolean[5][5];
+			for(int i = 0 ; i < 25 ; i++) {
+				if(num[i]) {
+					point = i;
+//					System.out.print(i/5+","+i%5+" " +map[i/5][i%5]);
+					visited[i/5][i%5]= true;
+				}
 			}
+			concnt= 1 ;
+			connect(point/5,point%5);
+
+		}
+		for(int i = start ; i < 25 ; i++) {
+			num[i] = true;
+			if(map[i/5][i%5].equals("Y")) {
+				select(num,cnt+1,i+1,cnty+1);
+			}else {
+				select(num,cnt+1,i+1,cnty);
+			}
+			num[i] = false;
+			
 		}
 		
 		
 		
 	}
 	
+	
+	// 뽑은 7개의 숫자가 서로 연결되어 있는지
+	static int concnt = 1 ;
+	public static void connect(int x,int y) {
+		//num을 몫과 나머지로 좌표를 구할 수 있다.
+
+		boolean flag = false;
+		visited[x][y] =false;
+		
+		for(int i = 0 ; i < 4 ; i++) {
+			int mx = x+a[i];
+			int my = y+b[i];
+			if(mx >=0 && my >= 0 && mx <5 && my <5) {
+				if(visited[mx][my]) {
+					concnt++;
+					connect(mx,my);
+					flag = true;
+				}
+			}
+			
+			
+		}
+		if(!flag ) {
+			if(concnt == 7) {
+				ans +=1;
+			}
+			
+			return ;
+		}
+		
+		
+	}
 	
 	
 }
