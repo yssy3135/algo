727f5f3ed44af984dd8924ee470c0ad40f37858c
ys3135 <ys3135@gmail.com>
2021-05-29 23:48:31 +0900

    백준 2293 동전1(dp)

---
+++ algo/src/solution270/Main.java
@@ -0,0 +1,37 @@
+package solution270;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+//백준 2293 동전1(dp)
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+
+        int k = Integer.parseInt(st.nextToken());
+
+        int arr[] = new int[n+1];
+        for(int i = 1 ; i<= n;i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        int dp[] = new int[k+1];
+        dp[0] = 1;
+        for(int i = 1 ; i <= n ;i++) {
+            for(int j = arr[i]; j <= k; j++) {
+                dp[j] += dp[j-arr[i]];
+            }
+        }
+
+        System.out.println(dp[k]);
+
+
+
+
+    }
+}
